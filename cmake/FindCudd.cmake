9d7c6050b5f76434e26544e19bd78866a389f8cf
Mateusz Rychlicki <you@example.com>
2022-04-27 13:32:23 +0200

    update FindCudd FindSylvan

--- cmake/FindCudd.cmake
+++ cmake/FindCudd.cmake
@@ -1,10 +1,16 @@
-message(mascot/fairsyn/cmake/FindCudd.cmake)
+message(cmake/FindCudd.cmake)
 find_package(PkgConfig)
 pkg_check_modules(PC_Cudd QUIET Cudd)
 
-find_path(Cudd_INCLUDE_DIR NAMES cudd.h uddObj.hh dddmp.h)
-find_library(Cudd_STATIC_LIB_DIR NAMES libcudd.a)
-find_library(Cudd_SHARED_LIB_DIR NAMES libcudd.so)
+if (CUDD_HOME)
+    find_path(Cudd_INCLUDE_DIR NAMES cudd.h uddObj.hh dddmp.h PATHS "${CUDD_HOME}/include" NO_DEFAULT_PATH)
+    find_library(Cudd_STATIC_LIB_DIR NAMES libcudd.a PATHS "${CUDD_HOME}/include" NO_DEFAULT_PATH)
+    find_library(Cudd_SHARED_LIB_DIR NAMES libcudd.so PATHS "${CUDD_HOME}/include" NO_DEFAULT_PATH)
+else ()
+    find_path(Cudd_INCLUDE_DIR NAMES cudd.h uddObj.hh dddmp.h)
+    find_library(Cudd_STATIC_LIB_DIR NAMES libcudd.a)
+    find_library(Cudd_SHARED_LIB_DIR NAMES libcudd.so)
+endif ()
 
 set(Cudd_VERSION ${PC_Cudd_VERSION})
 
@@ -16,17 +22,17 @@ find_package_handle_standard_args(Cudd
         VERSION_VAR Cudd_VERSION
         )
 
-if(Cudd_FOUND)
+if (Cudd_FOUND)
     #Set include dirs to parent, to enable includes like #include <rapidjson/document.h>
     get_filename_component(Cudd_INCLUDE_DIRS ${Cudd_INCLUDE_DIR} DIRECTORY)
-endif()
+endif ()
 
-if(Cudd_FOUND AND NOT TARGET Cudd::Cudd)
+if (Cudd_FOUND AND NOT TARGET Cudd::Cudd)
     add_library(Cudd::Cudd STATIC IMPORTED)
     set_target_properties(
             Cudd::Cudd
-                PROPERTIES
-                    IMPORTED_LOCATION ${Cudd_STATIC_LIB_DIR}
-                    INTERFACE_INCLUDE_DIRECTORIES "${Cudd_INCLUDE_DIRS}"
-            )
-endif()
+            PROPERTIES
+            IMPORTED_LOCATION ${Cudd_STATIC_LIB_DIR}
+            INTERFACE_INCLUDE_DIRECTORIES "${Cudd_INCLUDE_DIRS}"
+    )
+endif ()
--- cmake/FindSylvan.cmake
+++ cmake/FindSylvan.cmake
@@ -1,11 +1,16 @@
-message(mascot/fairsyn/cmake/FindSylvan.cmake)
-
+message(cmake/FindSylvan.cmake)
 find_package(PkgConfig)
 pkg_check_modules(PC_Sylvan QUIET Sylvan)
 
-find_path(Sylvan_INCLUDE_DIR NAMES sylvan.h)
-find_library(Sylvan_STATIC_LIB_DIR NAMES libSylvan.a)
-find_library(Sylvan_SHARED_LIB_DIR NAMES libSylvan.so)
+if (SYLVAN_HOME)
+    find_path(Sylvan_INCLUDE_DIR NAMES sylvan.h PATHS "${SYLVAN_HOME}/src" NO_DEFAULT_PATH)
+    find_library(Sylvan_STATIC_LIB_DIR NAMES libSylvan.a  PATHS "${SYLVAN_HOME}" NO_DEFAULT_PATH)
+    find_library(Sylvan_SHARED_LIB_DIR NAMES libSylvan.so  PATHS "${SYLVAN_HOME}" NO_DEFAULT_PATH)
+else ()
+    find_path(Sylvan_INCLUDE_DIR NAMES sylvan.h)
+    find_library(Sylvan_STATIC_LIB_DIR NAMES libSylvan.a)
+    find_library(Sylvan_SHARED_LIB_DIR NAMES libSylvan.so)
+endif()
 
 set(Sylvan_VERSION ${PC_Sylvan_VERSION})
 
@@ -17,18 +22,18 @@ find_package_handle_standard_args(Sylvan
         VERSION_VAR Sylvan_VERSION
         )
 
-if(Sylvan_FOUND)
+if (Sylvan_FOUND)
     #Set include dirs to parent, to enable includes like #include <Sylvan/document.h>
     get_filename_component(Sylvan_INCLUDE_DIRS ${Sylvan_INCLUDE_DIR} DIRECTORY)
-endif()
+endif ()
 
-if(Sylvan_FOUND AND NOT TARGET Sylvan::Sylvan)
+if (Sylvan_FOUND AND NOT TARGET Sylvan::Sylvan)
     add_library(Sylvan::Sylvan STATIC IMPORTED)
     set_target_properties(
             Sylvan::Sylvan
-                PROPERTIES
-                    IMPORTED_LOCATION ${Sylvan_STATIC_LIB_DIR}
-                    INTERFACE_INCLUDE_DIRECTORIES "${Sylvan_INCLUDE_DIRS}"
-            )
-endif()
+            PROPERTIES
+            IMPORTED_LOCATION ${Sylvan_STATIC_LIB_DIR}
+            INTERFACE_INCLUDE_DIRECTORIES "${Sylvan_INCLUDE_DIRS}"
+    )
+endif ()
 #add_link_options(--coverage)
