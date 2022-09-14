7acf8a9dbb6780ed6b20d55b8eda8fdccabb5718
Mateusz Rychlicki <you@example.com>
2022-09-14 13:20:51 +0200

    Applying acceleration from POPL_22_submission_#1

--- include/lib/BaseFixedPoint.hh
+++ include/lib/BaseFixedPoint.hh
@@ -34,9 +34,8 @@ namespace fairsyn {
         std::vector<size_t> *indexX;
 
         //////////////////// for acceleration
-        std::map<std::vector<size_t>, size_t> *permutation;
-        std::vector<std::map<std::vector<size_t>, UBDD>> *hist_Y;
-        std::vector<std::map<std::vector<size_t>, UBDD>> *hist_X;
+        std::vector<std::vector<std::vector<std::vector<UBDD>>>> *hist_Y;
+        std::vector<std::vector<std::vector<std::vector<UBDD>>>> *hist_X;
     };
 
     /**
@@ -121,21 +120,30 @@ namespace fairsyn {
             UBDD top = initial_seed;
             UBDD bot = base_.zero();
             /* the exact scheme as per the piterman paper */
-            std::vector<std::map<std::vector<size_t>, UBDD>> *hist_Y = new std::vector<std::map<std::vector<size_t>, UBDD>>;
-            std::vector<std::map<std::vector<size_t>, UBDD>> *hist_X = new std::vector<std::map<std::vector<size_t>, UBDD>>;
-            std::map<std::vector<size_t>, size_t> *permutation = new std::map<std::vector<size_t>, size_t>;
-
+            std::vector<std::vector<std::vector<std::vector<UBDD>>>> *hist_Y = new std::vector<std::vector<std::vector<std::vector<UBDD>>>>;
+            std::vector<std::vector<std::vector<std::vector<UBDD>>>> *hist_X = new std::vector<std::vector<std::vector<std::vector<UBDD>>>>;
             if (accl_on) {
                 /* if acceleration is on, then populate hist_Y and hist_X with the respective initial values */
                 /* the FIRST index is related to the depth, which is at most nrp+1 (the outer layer does not participate in the caching operation) */
-
-                *permutation = map_all_permutations(nrp);
-
-                for (int i = 0; i < (*permutation).size(); i++) {
-
-                    std::map<std::vector<size_t>, UBDD> temp;
-                    hist_Y->push_back(temp);
-                    hist_X->push_back(temp);
+                for (size_t i = 0; i < nrp; i++) {
+                    std::vector<std::vector<std::vector<UBDD>>> x, y;
+                    size_t npos = i + 1; /* the actual depth of the fixpoint variable, where the outermost variables have depth 0 */
+                    /* the SECOND index is the lexicographic position of the rabin pair subsequence 1...npos */
+                    for (size_t j = 0; j < factorial(nrp) / factorial(nrp - npos); j++) {
+                        //            for (size_t j=0; j<pow(nrp,npos); j++) {
+                        std::vector<std::vector<UBDD>> xx, yy;
+                        /* the THIRD index is the value of the sequence 0...npos-1 of the respective variables (Y sequence for hist_Y and X sequence for hist_X) */
+                        for (size_t k = 0; k < M; k++) {
+                            std::vector<UBDD> yyy(pow(M, npos), top); /* the FOURTH index for hist_Y is the value of the sequence 0...npos-1 of the X variables */
+                            yy.push_back(yyy);
+                            std::vector<UBDD> xxx(pow(M, npos + 1), bot); /* the FOURTH index for hist_X is the value of the sequence 0...npos of the Y variables */
+                            xx.push_back(xxx);
+                        }
+                        y.push_back(yy);
+                        x.push_back(xx);
+                    }
+                    hist_Y->push_back(y);
+                    hist_X->push_back(x);
                 }
             }
 
@@ -201,7 +209,6 @@ namespace fairsyn {
                                                                        indexRP,
                                                                        indexY,
                                                                        indexX,
-                                                                       permutation,
                                                                        hist_Y,
                                                                        hist_X};
                     //                    XX = RUN(RabinRecurse, this, &C, &arg_const_new, &arg_nconst_new);
@@ -246,6 +253,59 @@ namespace fairsyn {
             for (int i = 0; i < n; i++) {
                 std::cout << "\t";
             }
+        }
+
+        /**
+         *  @brief convert a number with base "base" to a decimal number position 0 is MSB
+         */
+        size_t to_dec(const size_t base, const std::vector<size_t> number) { // todo different to_dec implementation
+            size_t N = 0;
+            for (size_t i = 1; i <= number.size(); i++) {
+                //            N += number[i]*pow(base,i);
+                N += number[i - 1] * pow(base, number.size() - i);
+            }
+            return N;
+        }
+
+        /**
+         *  @brief compute the factorial
+         */
+        size_t factorial(const size_t n) {
+            if (n == 0) {
+                return 1;
+            } else {
+                return (n * factorial(n - 1));
+            }
+        }
+
+        /**
+         * @brief A utility function to count smaller characters on right of arr[low]
+         */
+        size_t findSmallerInRight(std::vector<size_t> str, size_t low) {
+            size_t countRight = str[low]; /* number of characters smaller than the chosen one */
+            for (size_t i = 0; i < low; i++) {
+                if (str[i] < str[low]) {
+                    countRight--;
+                }
+            }
+            return countRight;
+        }
+
+        /**
+         * @brief A function to find rank of a string in all permutations of characters
+         */
+        size_t findRank(size_t nsymbols, std::vector<size_t> str) { // todo check previous version of findRank.
+            size_t len = str.size();
+            size_t mul = factorial(len);
+            size_t rank = 0;
+            size_t countRight;
+            for (size_t i = 0; i < len; ++i) {
+                mul /= len - i;
+                // count number of chars smaller than str[i] from str[i+1] to str[len-1]
+                countRight = findSmallerInRight(str, i);
+                rank += countRight * mul;
+            }
+            return rank;
         }
 
         /**
