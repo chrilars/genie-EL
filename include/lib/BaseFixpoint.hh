e103d6a1720e95e5d281bf9f9786bb8f794d7b74
Mateusz Rychlicki <you@example.com>
2023-04-13 15:23:14 +0100

    add virtual

--- include/lib/BaseFixpoint.hh
+++ include/lib/BaseFixpoint.hh
@@ -380,7 +380,7 @@ namespace genie {
         /**
          *  @brief convert a number with base "base" to a decimal number position 0 is MSB
          */
-        size_t to_dec(const size_t base, const std::vector<size_t>& number) {
+        virtual size_t to_dec(const size_t base, const std::vector<size_t>& number) {
             size_t N = 0;
             for (size_t i = 1; i <= number.size(); i++) {
                 //            N += number[i]*pow(base,i);
