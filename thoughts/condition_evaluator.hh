7a5422d55059bb793c34af28d36a3ef37bed5aca
nils mo <nilsmo1@hotmail.com>
2024-04-24 17:42:15 +0200

    changes

--- thoughts/condition_evaluator.hh
+++ thoughts/condition_evaluator.hh
@@ -2,5 +2,5 @@
 
 #include <vector>
 inline bool condition_evaluator(const std::vector<bool>& vars) {
-    return (!vars[0] || (vars[1])) && ((!vars[0]) || (!vars[3])) && (vars[2]);
+    return vars[0];
 }
